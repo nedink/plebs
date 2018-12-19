78f83fdf134a6cf35435516f38cf20eb3b49dc37
David Bryant <nedink@gmail.com>
2018-12-19 09:50:47 -0500

    feat(plebs): update

--- pleb.js
+++ pleb.js
@@ -1,73 +1,189 @@
 class Pleb {
-  
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.vx = 0;
-    this.vy = 0;
+  constructor(x, y, p) {
+    this.p = p;
+    this.x = this.px = x;
+    this.y = this.py = y;
+    this.vx = this.pvx = 0;
+    this.vy = this.pvy = 0;
     this.shade = 100;
     this.target = null;
-    this.MAX_WALK = 1;
+    this.isAlive = true;
 
+    /**
+     * genetics
+     */
+    this.running = true;
+    this.fitness = 0;
+
+    // from parents
+    if (!p) {
+      this.stop = Math.random() * WIDTH;
+      this.jumpFreq = Math.pow(Math.random(), 10);
+      this.genome = [];
+      for (let i = 0; i < TIME; i++) {
+        this.genome.push(new Gene(Math.random() * 2 * Math.PI));
+      }
+    } else {
+      /**
+       * inherit
+       */
+      // stop
+      this.stop = p.stop;
+      if (Math.random() < 0.5) this.stop = p.stop;
+      // jump freq
+      this.jumpFreq = p.jumpFreq;
+      if (Math.random() < 0.5) this.jumpFreq = p.jumpFreq;
+      // vecs
+      this.genome = [];
+      for (let i = 0; i < p.genome.length; i++)
+        this.genome[i] = p.genome[i].copy();
+
+      /**
+       * mutate
+       */
+      let r = Math.pow(Math.random(), 10) * 100;
+      this.stop += Math.random() < 0.5 ? r : -r;
+
+      r = Math.pow(Math.random(), 10) * 100;
+      this.jumpFreq = p.jumpFreq;
+
+      // genome
+      this.genome.forEach((g, i) => {
+        if (Math.random() < MUT_RATE) {
+          this.genome[i].vec = random(Math.random() * Math.PI * 2);
+          this.genome[i].isMutant = true;
+        }
+      });
+    }
   }
 
   tx() {
-    return this.x;
+    return this.x - this.vx;
   }
 
   ty() {
     return this.y - 10;
   }
 
-  step() {
+  die() {
+    this.isAlive = false;
+  }
 
+  step() {
+    if (!this.isAlive) return;
     // target
-    if (Math.random() < 0.01) {
-      this.target = plebs[Math.floor(Math.random() * plebs.length)];
-    }
-    if (Math.random() < 0.01) {
-      this.target = null;
-    }
-    if (this.target) {
-      if (this.x < this.target.tx()) this.vx += 0.01;
-      else this.vx -= 0.01;
-      // this.vx += (this.target.tx() - this.x) * ;
+    // if (Math.random() < 0.01) {
+    //   this.target = plebs[Math.floor(Math.random() * plebs.length)];
+    // }
+    // if (Math.random() < 0.01) {
+    //   this.target = null;
+    // }
+    // if (this.target) {
+    //   if (this.x < this.target.tx()) this.vx += 0.01;
+    //   else this.vx -= 0.01;
+    // }
+
+    // jump
+    // if (Math.random() < 0.001) {
+    //   this.vy -= Math.random() * 4;
+    // }
+
+    /**
+     * velocity -----------------------------------------------------------
+     */
+
+    // set previous velocity
+    this.pvx = this.vx;
+    this.pvy = this.vy;
+
+    this.shade = 100;
+    this.vy += GRAV;
+
+    // stop for cliff
+    if (this.running) {
+      if (this.x < this.stop) {
+        // if (this.grounded) this.vx += ACC_GROUND;
+      } else {
+        this.running = false;
+      }
+    } else {
+      // this.vx = Math.max(0, this.vx - 0.01);
     }
 
     // jump
-    if (Math.random() < 0.001) {
-      this.vy -= Math.random() * 4;
+    if (this.jumpFreq < Math.random() && this.grounded) {
+      // this.vy -= JUMP_HEIGHT;
     }
 
-    // vel
-    this.shade = 100;
-    this.vy += GRAV
+    // do vecs
+    if (this.genome[frame] && this.grounded) {
+      const approx = this.genome[frame].vec;
+      // const approx = this.genome[frame].vec * (1 + Math.random() * 0.1);
+      // const approx = this.genome[frame].vec + (Math.random() * 0.01 - 0.005);
+      this.vx += Math.sin(approx);
+      this.vy += Math.cos(approx) * 4;
+    }
+
+    // fric
+    if (this.grounded) this.vx -= this.vx * FRIC;
+
     // max vel
-    if (this.vx < -this.MAX_WALK) this.vx = -this.MAX_WALK;
-    else if (this.vx > this.MAX_WALK) this.vx = this.MAX_WALK;
+    if (this.vx < -MAX_WALK) this.vx = -MAX_WALK;
+    else if (this.vx > MAX_WALK) this.vx = MAX_WALK;
 
-    // vel -> pos
-    this.x += this.vx
-    this.y += this.vy
+    /**
+     * position -----------------------------------------------------------
+     */
 
-    // collision
+    // set previous position
+    this.px = this.x;
+    this.py = this.y;
+
+    // add velocity
+    this.x += this.vx;
+    this.y += this.vy;
+
+    /**
+     * collision -----------------------------------------------------------
+     */
+
     // edges
     if (this.x < 0 || this.x > WIDTH) {
-      this.vx = -this.vx;
+      this.die();
+      return;
+      // this.vx = -this.vx;
     }
     // ground
-    const groundY = ground.getY(this.x);
-    if (this.y > groundY) {
-
-      // print(ground.vecs[0])
-      // print(ground.getY(this.y))
-
-      this.y = groundY;
-      this.vy = 0;
-      this.shade = 255;
-
-      // this.x = Math.random() * WIDTH;
-      // this.y = 0;
+    this.grounded = false;
+    if (ground.vecs.length) {
+      const groundY = ground.getY(this.x, this.y);
+      if (this.y > groundY) {
+        this.y = groundY;
+        this.vy = 0;
+        this.grounded = true;
+      }
     }
+    // hazard
+    if (hazard.vecs.length) {
+      const hazardY = hazard.getY(this.x, this.y);
+      if (this.y > hazardY) {
+        this.y = hazardY;
+        this.vy = 0;
+        this.shade = 255;
+        // TODO: spawn pleb bits
+      }
+    }
+
+    /**
+     * other -----------------------------------------------------------
+     */
+    this.fitness += this.isAlive ? 1 / Math.abs(tx - this.x) : 0;
+  }
+}
+
+class PlebBits {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,38 +1,243 @@
-
-
-
-const vecs = [];
-let x = -100, y = HEIGHT - 40 - Math.random() * 20;
-for (i = 0; i < 20; i++) {
-  vecs.push(new Vec(x, y, x += 100, y = HEIGHT - 40 - Math.random() * 20));
-}
-const ground = new Ground(vecs);
-const plebs = [];
-for (i = 0; i < 10; i++) {
-  plebs.push(new Pleb(Math.random() * WIDTH, Math.random() * HEIGHT));
+/**
+ * pause
+ */
+function pause() {
+  running = !running;
+  if (!running) noLoop();
+  else loop();
+  if (running) document.querySelector("#pause").innerHTML = `||`;
+  else document.querySelector("#pause").innerHTML = `>`;
 }
 
-// setup
+let targetHover = false;
+let targetClick = false;
+
+// timer
+let timer = 0;
+let frame = 0;
+
+let gVecs = [];
+let x = 0,
+  y = 400 + Math.random() * 20;
+for (i = 0; i < 6; i++) {
+  gVecs.push(new Vec(x, y, (x += 100), (y = 400 + Math.random() * 20)));
+}
+let ground = new Ground(gVecs);
+
+let tx = 300;
+let ty = 300;
+
+let hVecs = [];
+let hazard = new Hazard(hVecs);
+
+let plebs = [];
+for (i = 0; i < PLEB_COUNT; i++) {
+  plebs.push(new Pleb(10 + Math.random() * 20, HEIGHT));
+}
+
+let iteration = 1;
+
+let prvAvg = 0;
+let avg = 0;
+let best = 0;
+
+let outcomes = [];
+
+function mouseMoved() {
+  if (Math.abs(tx - mouseX) < 32) {
+    document.getElementsByTagName("body")[0].style.cursor = "-webkit-grab";
+  } else document.getElementsByTagName("body")[0].style.cursor = "default";
+}
+function mousePressed() {
+  if (Math.abs(tx - mouseX) < 32) {
+    targetClick = true;
+  }
+}
+function mouseDragged() {
+  if (targetClick) {
+    document.getElementById("target-indicator").value = mouseX / 6;
+    document.getElementsByTagName("body")[0].style.cursor = "-webkit-grabbing";
+    tx = 6 * document.getElementById("target-indicator").value;
+  }
+}
+function mouseReleased() {
+  targetClick = false;
+}
+
+/**
+ * initial state
+ */
+function reset() {
+  // timer
+  timer = 0;
+  frame = 0;
+
+  gVecs = [];
+  (x = 0), (y = 400 + Math.random() * 20);
+  for (i = 0; i < 6; i++) {
+    gVecs.push(new Vec(x, y, (x += 100), (y = 400 + Math.random() * 20)));
+  }
+  ground = new Ground(gVecs);
+
+  // tx = 300;
+  // ty = 300;
+  // console.log(tx);
+
+  hVecs = [];
+  // x = 0;
+  // y = 400 + Math.random() * 20;
+  // for (i = 0; i < 8; i++) {
+  //   hVecs.push(new Vec(x, y, x += 100, y = 400 + Math.random() * 20));
+  // }
+  hazard = new Hazard(hVecs);
+
+  plebs = [];
+  for (i = 0; i < PLEB_COUNT; i++) {
+    plebs.push(new Pleb(10 + Math.random() * 20, HEIGHT));
+  }
+
+  iteration = 1;
+
+  prvAvg = 0;
+  avg = 0;
+  best = 0;
+
+  outcomes = [];
+}
+
+/**
+ * runs for a number of iterations
+ * @param {Number} iterations
+ */
+
+function run(iterations) {
+  for (let i = 0; i < iterations; i++) {
+    // pause
+    // if (!running) return;
+
+    // time
+    frame++;
+
+    // exit
+    if (frame > 10) window.stop();
+
+    if (frame >= TIME) {
+      timer = 0;
+      frame = 0;
+
+      // record outcomes
+      for (const pleb of plebs) {
+        outcomes.push(pleb.x);
+      }
+      // get updated avg
+      const sum = plebs.map(pleb => pleb.x).reduce((acc, cur) => acc + cur);
+      prvAvg = avg;
+      avg = sum / plebs.length;
+      // get updated best
+      best = plebs.sort((a, b) => a.fitness > b.fitness)[0];
+
+      /**
+       * do next gen -----------------------------------------------------------
+       */
+
+      plebs.sort((a, b) => b.fitness - a.fitness);
+
+      plebs.forEach((pleb, i) => {
+        pleb.r = Math.pow(0.8, i);
+      });
+
+      newPlebs = [];
+      while (newPlebs.length < PLEB_COUNT) {
+        plebs.sort((a, b) => a.r - b.r);
+
+        let p;
+        const r = Math.random();
+
+        for (const pleb of plebs) {
+          if (r < pleb.r) {
+            p = pleb;
+            break;
+          }
+        }
+
+        newPleb = new Pleb(10 + Math.random() * 20, HEIGHT, p);
+
+        newPlebs.push(newPleb);
+      }
+
+      plebs = newPlebs.slice();
+      iteration++;
+    }
+
+    // update plebs
+    for (pleb of plebs) {
+      pleb.step();
+    }
+  }
+}
+
+/**
+ * SETUP -----------------------------------------------------------
+ */
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
 
-  for (vec of ground.vecs) {
-    stroke(255);
-  }
+  reset();
+  running = true;
 }
 
+function doEvents() {
+  if (!targetClick)
+    tx = 6 * parseInt(document.querySelector("#target-indicator").value);
+  MUT_RATE = parseFloat(document.querySelector("#mutation-rate").value);
+}
 
-
-// draw
+/**
+ * DRAW -----------------------------------------------------------
+ */
 function draw() {
-  
+  run(1);
+
+  doEvents();
+
+  /**
+   * draw -----------------------------------------------------------
+   */
   background(0);
 
-  // update plebs
-  for (pleb of plebs) {
-    pleb.step();
-  }
+  // draw info
+  fill(255);
+  noStroke();
+  // iteration
+  text("Iteration: " + iteration, 10, 20);
+  // time
+  text("Time left (sec): " + (TIME - frame) / 100, 10, 40);
+  // FPS
+  // text("FPS: " + Math.floor(frameCount), 10, 60);
+  // frame
+  text("Frame: " + frame, 10, 60);
+
+  // target
+  stroke(0, 180, 0);
+  strokeWeight(Math.abs(mouseX - tx) < 32 ? 2 : 1);
+  line(tx, 0, tx, ty);
+  line(tx, ty, tx - 10, ty - 10);
+  line(tx, ty, tx + 10, ty - 10);
+
+  strokeWeight(1);
+
+  // average
+  stroke(255, 25500 * (1 / Math.abs(tx - avg)), 0);
+  line(avg, 0, avg, HEIGHT);
+  fill(255, 25500 * (1 / Math.abs(tx - avg)), 0);
+  noStroke();
+  text("Average", avg + 8, ground.getY(avg) - 40);
+  stroke(255, 25500 * (1 / Math.abs(tx - best)), 0);
+  // line(best, 0, best, HEIGHT);
+  fill(255, 25500 * (1 / Math.abs(tx - best)), 0);
+  noStroke();
+  // text("Best", best + 8, HEIGHT);
 
   // draw ground
   for (vec of ground.vecs) {
@@ -40,19 +245,53 @@ function draw() {
     line(vec.sx, vec.sy, vec.ex, vec.ey);
   }
 
+  // draw hazard
+  for (vec of hazard.vecs) {
+    stroke(255, 0, 0);
+    line(vec.sx, vec.sy, vec.ex, vec.ey);
+  }
+
   // draw plebs
   for (pleb of plebs) {
+    // if (!pleb.isAlive) continue;
 
     // draw pleb
-    stroke(pleb.shade);
-    line(pleb.x, pleb.y, pleb.x, pleb.ty());
+    stroke(255);
+    if (!pleb.isAlive)
+      stroke(255, 0, 0);
+    if (pleb.genome[frame] && pleb.genome[frame].isMutant){
+      // text("!", pleb.x, pleb.y - 20);
+      stroke(255, 0, 255);
+      // point(pleb.x, pleb.y - 20);
+    } 
+    
+    // if (pleb.genome[frame] && pleb.genome[frame].isMutant) stroke(255, 0, 0);
+    line(pleb.x, pleb.y, pleb.tx(), pleb.ty());
+
+    // pleb info
+
+    // text(Math.floor(100 * pleb.vx) / 100, pleb.tx(), pleb.ty() - 10);
+    // text(Math.floor(pleb.fitness) / 100, pleb.tx(), pleb.ty() - 10);
+
+    let fittest = true;
+    for (p of plebs) {
+      if (pleb === p) continue;
+      if (p.fitness > pleb.fitness) {
+        fittest = false;
+      }
+    }
+    if (fittest) {
+      stroke(Math.abs(tx - pleb.x), 255 * pleb.fitness * 100, 0);
+      fill(255);
+      text(pleb.fitness, pleb.tx(), pleb.ty() - 10);
+    }
+    // stroke(Math.abs(tx - pleb.x), 255 * pleb.fitness * 100, 0);
+    // text(Math.floor(10000 * pleb.fitness) / 10000, pleb.tx(), pleb.ty() - 10);
 
     // draw target
-    if (pleb.target) {
-      stroke(255, 0, 0)
-      line(pleb.x, pleb.y - 10, pleb.target.tx(), pleb.target.ty());
-    }
+    // if (pleb.target) {
+    //   stroke(255, 0, 0)
+    //   line(pleb.x, pleb.y - 10, pleb.target.tx(), pleb.target.ty());
+    // }
   }
-  
-
 }
